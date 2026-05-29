05f28f00f0035f0f4d9f5ef24fd96ffcd9faba7d
kazu0112 <kazuma0112@icloud.com>
2026-05-29 16:11:15 +0900

    LoginDAOクラス修正

--- src/main/java/com/diworksdev/login/dao/LoginDAO.java
+++ src/main/java/com/diworksdev/login/dao/LoginDAO.java
@@ -3,6 +3,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import com.diworksdev.login.dto.LoginDTO;
 import com.diworksdev.login.util.DBConnector;
 
@@ -11,26 +12,26 @@ public class LoginDAO {
 		LoginDTO dto=new LoginDTO();
 		DBConnector db = new DBConnector();
 		Connection con = db.getConnection();
-	}
 	
 	try {
 		String sql="select * from user where user_name=? and password=?";
 	
 		PreparedStatement ps = con.prepareStatement(sql);
+		
 		ps.setString(1, name);
 		ps.setString(2, password); 
+		
 		ResultSet rs=ps.executeQuery();
-	}
 	
 		if(rs.next()){
 			dto.setName(rs.getString("user_name"));
 			dto.setPassword(rs.getString("password"));
 		}
 	
-		} catch (SQLException e)
-			{ e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
 			} finally {
-			con.close();
+				con.close();
 			}
 
 			return dto;
